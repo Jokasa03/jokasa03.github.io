3589e86371602207de54b010e8293c0b09e6de2f
josesc03 <132373225+Jokasa03@users.noreply.github.com>
2024-06-05 01:49:15 +0200

    commit ahora funciona

--- lovepage/scripts/scripts.js
+++ lovepage/scripts/scripts.js
@@ -1,52 +1,45 @@
-var mensaje  = document.getElementById("message");
+var imagen = document.getElementById("imagen");
+var mensaje = document.getElementById("message");
 
 document.getElementById("si").addEventListener("click", function () {
     mensaje.innerHTML = "Oleeeee ♥️♥️♥️♥️";
-    document.getElementById("imagen").style.backgroundImage = "url(../resources/love.gif)";
+    imagen.style.backgroundImage = "url(./resources/love.gif)";
     document.getElementById("no").disabled = true;
-
-}) 
+});
 
 document.getElementById("no").addEventListener("click", function () {
     mensaje.innerHTML = "Como que no :((((";
-    document.getElementById("imagen").style.backgroundImage = "url(../resources/sad-broken-heart.gif)";
+    imagen.style.backgroundImage = "url(./resources/sad-broken-heart.gif)";
     document.getElementById("si").disabled = true;
-
-}) 
+});
 
 function random(number) {
-    let randomnumber = -1;
-    while (randomnumber < document.getElementById("no").translate) {
-         randomnumber = Math.floor(Math.random() * (number + 1));
-    }
-    return randomnumber;
+    return Math.floor(Math.random() * (number + 1));
 }
 
 const no = document.getElementById("no");
 
-no.addEventListener("mouseover", crearBarrera, true)
+no.addEventListener("mouseover", crearBarrera, true);
 
 var creada = false;
 
 var barreraNo = document.createElement("div");
+
 function crearBarrera() {
     if (!creada) {
-        removeEventListener("mouseover", crearBarrera, true);
         no.appendChild(barreraNo);
         barreraNo.style.position = "absolute";
         barreraNo.style.width = "150px";
         barreraNo.style.height = "100px";
         barreraNo.style.top = "-105%";
-        barreraNo.style.left = "-72 %";
+        barreraNo.style.left = "-72%";
         creada = true;
     }
 }
 
 barreraNo.addEventListener("mouseover", () => {
     if (!no.disabled) {
-        no.style.scale = 0.6;
-        no.style.transform = "translate("+ random(800) +"px, " + random(300) +"px)";
-
+        no.style.transform = "scale(0.6) translate(" + random(800) + "px, " + random(300) + "px)";
         no.style.backgroundColor = 'rgb(244, 166, 180)';
         no.style.color = 'white';
         no.style.fontWeight = '700';
@@ -54,9 +47,9 @@ barreraNo.addEventListener("mouseover", () => {
 });
 
 barreraNo.addEventListener("mouseout", () => {
-        no.style.backgroundColor = '';
-        no.style.color = '';
-        no.style.fontWeight = '';
+    no.style.backgroundColor = '';
+    no.style.color = '';
+    no.style.fontWeight = '';
 });
 
 document.getElementById("si").addEventListener('mouseover', () => {
@@ -68,7 +61,7 @@ document.getElementById("si").addEventListener('mouseover', () => {
     }
 });
 
-document.getElementById("si").addEventListener('mouseout', () => {
+document.getElementById("si").addEventListener('mouseout', function() {
     if (!this.disabled) {
         document.getElementById("si").style.backgroundColor = '';
         document.getElementById("si").style.color = '';
@@ -76,4 +69,3 @@ document.getElementById("si").addEventListener('mouseout', () => {
         document.getElementById("si").style.transform = '';
     }
 });
-
