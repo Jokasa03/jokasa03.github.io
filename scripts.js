e0ee7e50ad5bd7f3706f09dd8f0166d9ab048791
josesc03 <132373225+Jokasa03@users.noreply.github.com>
2024-06-05 05:27:22 +0200

    scrollupdatemain

--- scripts.js
+++ scripts.js
@@ -1,23 +1,17 @@
 document.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY + window.innerHeight;
+    var scrollPosition = window.scrollY;
 
     console.log(scrollPosition);
 
-    if (scrollPosition > 720) {
+    if (scrollPosition > 0) {
         document.querySelector('header').style.backgroundColor = 'rgb(0, 0, 0, .2)';
         document.querySelector('header').style.backdropFilter = 'blur(2px)';
         document.querySelector('header').style.height = '50px';
         document.querySelector('header').style.fontSize = '2vw';
-
-        document.querySelector('main').style.marginTop = '8vw';
     } else {
         document.querySelector('header').style.backgroundColor = '';
         document.querySelector('header').style.backdropFilter = '';
         document.querySelector('header').style.height = '';
         document.querySelector('header').style.fontSize = '';
-
-        document.querySelector('main').style.marginTop = '';
-
-
     }
 });
